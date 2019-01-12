fcc8c163d241c6d38c501a513a2bbc127a0187e8
Madalina Caea <mada.caea@gmail.com>
2019-01-12 09:19:43 +0000

    url

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,15 @@ from django.conf.urls import url
 from views import (
     index,
     article_detail,
+    all_articles,
+    all_categories,
+    CategoryCreateView,
     )
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^article/(?P<pk>[0-9]+)$', article_detail, name='article_detail'),
+    url(r'^article/all$',all_articles,name='allarticles'),
+    url(r'^category/all$',all_categories,name='allcategories'),
+    url(r'^category/create$',CategoryCreateView.as_view(),name='category_create'),
     ]
